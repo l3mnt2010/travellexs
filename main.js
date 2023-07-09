faf2ad7385d9062247737b6b6b2daae35e5d5c2c
llamtruongdev365 <126931874+llamtruongdev365@users.noreply.github.com>
2023-07-09 18:45:40 +0700

    feat : add all

---
+++ main.js
@@ -0,0 +1,37 @@
+// let img1 = document.getElementById("img1");
+// let img2 = document.getElementById("img2");
+// let img3 = document.getElementById("img3");
+// let img4 = document.getElementById("img4");
+// setTimeout(mySwap(0), 5000);
+
+// function mySwap(num){
+//     if(num%4==0){
+//         img1.style.backgroundImage ="url('./images/xoffer_1.jpg.pagespeed.ic.LnLHOsFCJq.webp')"
+//         img2.style.backgroundImage ="url('./images/xoffer_2.jpg.pagespeed.ic.HA9cJExNCg.webp')"
+//         img3.style.backgroundImage ="url('./images/xoffer_3.jpg.pagespeed.ic.9JfYNcm8fb.webp')"
+//         img4.style.backgroundImage ="url('./images/xoffer_4.jpg.pagespeed.ic.sP-uVX8-UR.webp')"
+//         num++;
+//     }
+//     if(num%4==1){
+//         img2.style.backgroundImage ="url('./images/xoffer_1.jpg.pagespeed.ic.LnLHOsFCJq.webp')"
+//         img3.style.backgroundImage ="url('./images/xoffer_2.jpg.pagespeed.ic.HA9cJExNCg.webp')"
+//         img4.style.backgroundImage ="url('./images/xoffer_3.jpg.pagespeed.ic.9JfYNcm8fb.webp')"
+//         img1.style.backgroundImage ="url('./images/xoffer_4.jpg.pagespeed.ic.sP-uVX8-UR.webp')"
+//         num++;
+//     }
+//     if(num%4==2){
+//         img3.style.backgroundImage ="url('./images/xoffer_1.jpg.pagespeed.ic.LnLHOsFCJq.webp')"
+//         img4.style.backgroundImage ="url('./images/xoffer_2.jpg.pagespeed.ic.HA9cJExNCg.webp')"
+//         img1.style.backgroundImage ="url('./images/xoffer_3.jpg.pagespeed.ic.9JfYNcm8fb.webp')"
+//         img2.style.backgroundImage ="url('./images/xoffer_4.jpg.pagespeed.ic.sP-uVX8-UR.webp')"
+//         num++;
+
+//     }
+//     if(num%4==3){
+//         img4.style.backgroundImage ="url('./images/xoffer_1.jpg.pagespeed.ic.LnLHOsFCJq.webp')"
+//         img1.style.backgroundImage ="url('./images/xoffer_2.jpg.pagespeed.ic.HA9cJExNCg.webp')"
+//         img2.style.backgroundImage ="url('./images/xoffer_3.jpg.pagespeed.ic.9JfYNcm8fb.webp')"
+//         img3.style.backgroundImage ="url('./images/xoffer_4.jpg.pagespeed.ic.sP-uVX8-UR.webp')"
+//         num++;
+//     }
+// }
